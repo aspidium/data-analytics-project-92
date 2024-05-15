965b0e10f2f46abb9bd674c9b41a930744d5b611
aspidium <74487757+aspidium@users.noreply.github.com>
2024-05-15 10:09:46 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
--- This query returns count of customers
+--Проект Продажи
+--4.Работа с базой данных
+--запрос, который считает общее количество покупателей из таблицы customers
 select count(customer_id) as customers_count
 from salesdb.public.customers c;
 
+--5.Анализ отдела продаж
+--Первый отчет о десятке лучших продавцов. 
 select 
 CONCAT(e.first_name,' ', e.last_name) as seller, -- объединение имени и фамилии в одной ячейке
 count(s.sales_id) as operations, --  количество проведенных сделок
@@ -13,6 +17,7 @@ group by 1 -- группировка по seller
 order by 3 desc --сортировка в обратном порядке
 limit 10; --вывод первых 10
 
+--Второй отчет содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам. 
 select 
 CONCAT(e.first_name,' ', e.last_name) as seller, -- объединение имени и фамилии в одной ячейке
 floor(avg(p.price * s.quantity)) as average_income -- округление в меньшую сторону средней выручки каждого продавца
@@ -25,6 +30,7 @@ having floor(avg(p.price * s.quantity)) <( select floor(avg(p.price * s.quantity
       left join salesdb.public.products p on p.product_id = s.product_id) -- сравнение среднего значения выручки за сделку каждого продавца со средней выручкой за сделку по всем продавцам.
       order by 2;
 
+--Третий отчет содержит информацию о выручке по дням недели.
 with dat as (SELECT
                CONCAT(e.first_name,' ', e.last_name) AS seller, -- объединение имени и фамилии в одной ячейке
                TO_CHAR(s.sale_date, 'day') AS day_of_week,  -- преобразование число или дату в строку
@@ -41,3 +47,17 @@ day_of_week,
 income
 FROM dat
 ORDER by dow,seller;
+
+--6.Анализ покупателей
+--Первый отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+.
+select 
+( CASE -- использование оператора CASE для обработки и выполнения условий
+            WHEN c.age between 16 and 25 THEN '16-25'
+            WHEN c.age between 26 and 40 THEN '26-40'
+            ELSE '40+' 
+        END
+    ) AS age_category,
+    COUNT(c.customer_id) AS age_count --количество покупателей 
+FROM salesdb.public.customers c
+group by 1
+order by 1;
